fd5dfd2a03ce7f229167d39b0edbe05cf7a0cb54
Pedro Vieira <pedro.vieira@dremio.com>
2026-06-10 20:07:23 +0100

    Apply swift-format formatting to all source files

--- Sources/PiHoleMenuApp/PiHoleMenuApp.swift
+++ Sources/PiHoleMenuApp/PiHoleMenuApp.swift
@@ -1,23 +1,24 @@
-import SwiftUI
 import AppKit
+import SwiftUI
 
 @main
 struct PiHoleMenuApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  @NSApplicationDelegateAdaptor(AppDelegate.self)
+  var appDelegate
 
-    var body: some Scene {
-        Settings {
-            Text("Settings")
-                .frame(width: 300, height: 200)
-        }
+  var body: some Scene {
+    Settings {
+      Text("Settings")
+        .frame(width: 300, height: 200)
     }
+  }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    private var menuBarController: MenuBarController?
+  private var menuBarController: MenuBarController?
 
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.accessory)
-        menuBarController = MenuBarController()
-    }
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    NSApp.setActivationPolicy(.accessory)
+    menuBarController = MenuBarController()
+  }
 }
