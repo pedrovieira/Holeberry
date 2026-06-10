6846543a0e74d511180acf0926d7b71a5298e4be
Pedro Vieira <pedro.vieira@dremio.com>
2026-06-10 20:05:39 +0100

    Add SwiftLint and swift-format configuration with build phase scripts

--- Package.swift
+++ Package.swift
@@ -2,18 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "PiHoleMenuApp",
-    platforms: [
-        .macOS(.v14)
-    ],
-    targets: [
-        .executableTarget(
-            name: "PiHoleMenuApp",
-            exclude: ["Info.plist", "Resources"]
-        ),
-        .testTarget(
-            name: "PiHoleMenuAppTests",
-            dependencies: ["PiHoleMenuApp"]
-        )
-    ]
+  name: "PiHoleMenuApp",
+  platforms: [
+    .macOS(.v14)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/realm/SwiftLint", from: "0.55.0")
+  ],
+  targets: [
+    .executableTarget(
+      name: "PiHoleMenuApp",
+      exclude: ["Info.plist", "Resources"],
+      plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
+    ),
+    .testTarget(
+      name: "PiHoleMenuAppTests",
+      dependencies: ["PiHoleMenuApp"],
+      plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
+    )
+  ]
 )
