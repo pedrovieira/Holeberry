79d3553673810d6dc02f5df49ab74b51b96bd81c
Pedro Vieira <pedrovieiradev@hotmail.com>
2026-06-17 21:21:28 +0100

    feat: add ShortcutNames extension (checkpoint 7)

---
+++ Sources/PiHoleMenuApp/Utils/ShortcutNames.swift
@@ -0,0 +1,10 @@
+import KeyboardShortcuts
+
+extension KeyboardShortcuts.Name {
+    static let disableIndefinitely = Self("disableIndefinitely")
+    static let disable10s = Self("disable10s")
+    static let disable30s = Self("disable30s")
+    static let disable5m = Self("disable5m")
+    static let disableCustom = Self("disableCustom")
+    static let reEnableBlocking = Self("reEnableBlocking")
+}
